11830c7d0881357dd8e6f875ad964bbf347afb2b
LeeMboya <117488711+LeeMboya@users.noreply.github.com>
2022-12-19 18:22:35 +0300

    Update and rename 0x04-pointers_arrays_strings/0-reset_to_98.c to 0x05-pointers_arrays_strings/0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 /*
  * File: 0-reset_to_98.c
- * Auth: Brennan D Baraban
+ * Auth: Tracy Wankio
  */
 
-#include "holberton.h"
+#include "main.h"
 
 /**
  * reset_to_98 - Takes a pointer to an int and updates
