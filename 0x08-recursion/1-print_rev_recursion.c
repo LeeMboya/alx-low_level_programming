be566d0dc6268a4933a03abb7674ec3bf370a528
LeeMboya <117488711+LeeMboya@users.noreply.github.com>
2023-01-04 16:05:08 +0300

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,9 +1,9 @@
 /*
  * File: 1-print_rev_recursion.c
- * Auth: Brennan D Baraban
+ * Auth: Leeroy Mboya
  */
 
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _print_rev_recursion - Prints a string in reverse.
